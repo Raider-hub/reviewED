756823a89727d62abc29f5b336779b6128fdc73a
Raider-hub <timedoula@gmail.com>
2024-09-19 17:31:40 +0100

    configure serverless-http for vercel

---
+++ api/server.js
@@ -0,0 +1,18 @@
+
+
+
+// api/server.js
+const express = require('express');
+const serverless = require('serverless-http');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Handle all routes by serving the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+module.exports.handler = serverless(app);
